6bd51b99bd2bbcd868813b9bc8dc1dcc9cd0ac74
tushartambatkar <tushartambatkar777@gmail.com>
2024-05-04 11:57:54 +0530

    code commit

---
+++ Decreasing.java
@@ -0,0 +1,17 @@
+package Recursion;
+
+public class Decreasing{
+  public static void printDec(int n) {
+    //Base case
+    if(n==1) {
+      System.out.println(n);
+      return;
+    }
+    System.out.print(n+" ");
+    printDec(n-1);
+  }
+  public static void main(String args[]) {
+    int n = 10;
+    printDec(n);
+  }
+}
