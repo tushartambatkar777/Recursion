2c827595a82bdf03fe160e8fc633b40c4db37140
tushartambatkar <tushartambatkar777@gmail.com>
2024-05-04 17:43:38 +0530

    code commit

---
+++ Lastoccurence.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+public class Lastoccurence {
+  public static int lastOccurence(int arr[], int key, int i) {
+    if(i == arr.length) {
+      return -1;
+    }
+
+    int isFound = lastOccurence(arr, key, i+1);
+    if(isFound == -1 && arr[i] == key) {
+      return i;
+    }
+    return isFound;
+  }
+  public static void main(String args[]) {
+    int arr[] = {8, 3, 6, 9, 5, 10, 2, 5, 3}; 
+    System.out.println(lastOccurence(arr, 5, 0));
+  }
+}
+
+// Time = O(n);
+// Space = O(n);
