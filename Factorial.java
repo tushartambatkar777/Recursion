60cb0913a9caf79dc3877dc6ea878a4ad13780bc
tushartambatkar <tushartambatkar777@gmail.com>
2024-05-04 13:35:02 +0530

    code commit

---
+++ Factorial.java
@@ -0,0 +1,20 @@
+package Recursion;
+
+public class Factorial {
+  public static int fact(int n) {
+    if(n==0) {
+      return 1;
+    }
+
+    int fnm1 = fact(n-1);
+    int fn = n * fact(n-1);
+    return fn;
+  }
+  public static void main(String args[]) {
+    int n = 5;
+    System.out.println("Factorial is: "+ fact(n));
+  }
+}
+
+// Time = O(n);
+// Space = O(n);
